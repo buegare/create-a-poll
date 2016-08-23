7d16928571ff4b089a4d133e44ef45465bd2bda1
buegare <brunobuegare@gmail.com>
2016-08-23 11:26:32 -0300

    Configure models relations and revert migration

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20160822143105) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_polls", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "poll_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
