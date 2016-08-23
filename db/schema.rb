e2961a5ca1c34a5eb195757321232b6a01327fb4
buegare <brunobuegare@gmail.com>
2016-08-23 11:35:38 -0300

    Add column user_id to Poll model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822142717) do
+ActiveRecord::Schema.define(version: 20160823143354) do
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
