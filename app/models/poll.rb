95c2486d040bb0b3b58d0453581b1f068c12052a
buegare <brunobuegare@gmail.com>
2016-08-23 12:49:55 -0300

    Add column poll_id to Question model

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,4 @@
 class Poll < ApplicationRecord
 	belongs_to :user
-	has_many :questions
+	has_many :questions, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823143354) do
+ActiveRecord::Schema.define(version: 20160823143612) do
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160823143354) do
     t.integer  "votes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "poll_id"
   end
 
   create_table "users", force: :cascade do |t|
